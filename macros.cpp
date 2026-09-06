f1c5f7b6977a37114721d13fe92170d51c636e4b
Erasmo Junior <essj@ic.ufal.br>
2026-09-05 21:21:21 -0300

    aumenta lista de ignorados em .gitignore e adiciona lista de macros

---
+++ macros.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vvpi vector<vector<pair<ll,ll>>>
+#define vvi vector<vector<ll>>
+#define vpi vector<pair<ll,ll>>
+#define vi vector<ll>
+#define pi pair<ll,ll>
+#define fi first
+#define si second
+#define pb push_back
+#define loop(i, x, f) for(int i=x; i<f; i++)
+
+using namespace std;
+
+signed main()
+{
+
+}
